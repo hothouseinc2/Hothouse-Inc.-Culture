57f1774f220bf0780bca6303f6b303aec6291313
Benjamin Modayil <modayilme@gmail.com>
2018-07-05 13:19:17 -0400

    template for GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,20 @@ module.exports = {
     title: 'Gatsby Starter Tailwind'
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/']
+      }
+    },
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-netlify` // make sure to put last in the array
