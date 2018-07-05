dc4fcb20cb9318ccdae8b47cbb0042bf4f4efef5
Benjamin Modayil <modayilme@gmail.com>
2018-07-05 12:28:31 -0400

    init netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Tailwind',
+    title: 'Gatsby Starter Tailwind'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-};
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify` // make sure to put last in the array
+  ]
+}
