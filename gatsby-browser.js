3037fa342f0b68829636ea9e918ff6a98838302e
Benjamin Modayil <modayilme@gmail.com>
2018-08-09 13:15:58 -0400

    required object-fit in browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -21,12 +21,12 @@ exports.onClientEntry = () => {
   }
 
   // Object-fit/Object-position polyfill for gatsby-image (IE)
-  // const testImg = document.createElement(`img`)
-  // if (
-  //   typeof testImg.style.objectFit === `undefined` ||
-  //   typeof testImg.style.objectPosition === `undefined`
-  // ) {
-  //   require(`object-fit-images`)()
-  //   console.log(`👍 Object-fit/Object-position are polyfilled`)
-  // }
+  const testImg = document.createElement(`img`)
+  if (
+    typeof testImg.style.objectFit === `undefined` ||
+    typeof testImg.style.objectPosition === `undefined`
+  ) {
+    require(`object-fit-images`)()
+    console.log(`👍 Object-fit/Object-position are polyfilled`)
+  }
 }
